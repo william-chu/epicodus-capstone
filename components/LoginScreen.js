abdc0347eec40600f832c6498cd8c0990f02d14c
William Chu <williamchu51@gmail.com>
2018-07-13 12:32:59 -0700

    add login button image

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -2,33 +2,37 @@ import React from 'react';
 import { StyleSheet, Button, Text, Image, View } from 'react-native';
 import { LinearGradient } from 'expo';
 import stomachImg from '../assets/images/stomach.png';
+import loginBtn from '../assets/images/loginbtn.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#6F62AB',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  slogan: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    marginLeft: 30,
+    marginTop: 10,
+  }
 });
 
 export default class LoginScreen extends React.Component {
   render() {
     return (
       <LinearGradient
-        colors={['#6F62AB', '#B0A1F2']}
-        style = { styles.container }>
+        colors={['#B0A1F2', '#6F62AB']}
+        style = {styles.container}>
         <View>
           <Image
             resizeMode={'cover'}
-            source={stomachImg}
-            style={styles.canvas} />
-          <Text>Discover your dietary triggers</Text>
-          <Text>for better digestive health</Text>
-          <Button
-            title="Login"
-            onPress={() => this.props.navigation.navigate('Home')}
-          />
+            source={stomachImg} />
+          <Text style = {styles.slogan}>{`
+            Discover your dietary triggers
+            for better digestive health
+          `}</Text>
+          <Button title='Login' onPress={() => this.props.navigation.navigate('Home')} />
+          <Image source={loginBtn} />
         </View>
       </LinearGradient>
     );
