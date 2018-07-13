3f3bfe605fc2509177edc28f3541baffb762b011
William Chu <williamchu51@gmail.com>
2018-07-13 11:04:13 -0700

    begin implementing react navigation

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, Image, View } from 'react-native';
 import { LinearGradient } from 'expo';
-// import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
 
 import stomachImg from './assets/images/stomach.png';
 
-// const App = createStackNavigator({
-//   Home: { screen: HomeScreen },
-//   Profile: { screen: ProfileScreen },
-// });
-
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render() {
     return (
       <LinearGradient
@@ -29,28 +24,30 @@ export default class App extends React.Component {
   }
 }
 
-// class HomeScreen extends React.Component {
-//   static navigationOptions = {
-//     title: 'Login',
-//   };
-//   render() {
-//     const { navigate } = this.props.navigation;
-//     return (
-//       <LinearGradient
-//         colors={['#6F62AB', '#B0A1F2']}
-//         style = { styles.container }>
-//         <View>
-//           <Image
-//             resizeMode={'cover'}
-//             source={stomachImg}
-//             style={styles.canvas} />
-//           <Text>Discover your dietary triggers</Text>
-//           <Text>for better digestive health</Text>
-//         </View>
-//       </LinearGradient>
-//     );
-//   }
-// }
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator({
+  Home: HomeScreen,
+  Details: DetailsScreen,
+ },
+ {
+   initialRouteName: 'Home',
+ }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -59,7 +56,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  canvas: {
-
-  }
 });
