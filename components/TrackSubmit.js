91cec0c1c7c163d5166fd8dec2a3ba36b86558bd
William Chu <williamchu51@gmail.com>
2018-07-21 13:03:21 -0700

    change image imports for tracksubmit to troubleshoot expo android emulator issue

--- components/TrackSubmit.js
+++ components/TrackSubmit.js
@@ -9,10 +9,6 @@ import {
 import { LinearGradient } from 'expo';
 import styles from './styles';
 import { genMealLogDateKey } from './helper';
-import goodResult from '../assets/images/goodresult.png';
-import okayResult from '../assets/images/okayresult.png';
-import badResult from '../assets/images/badresult.png';
-import footer from '../assets/images/footer.png';
 
 const compStyles = StyleSheet.create({
   // Component Specific Styles Go Here
@@ -64,19 +60,19 @@ export default function TrackSubmit(props) {
   let actionText = <Text>Select Analyze from the main page for further information.</Text>;
   let specialText = null;
   if (scaleInput === 1 || scaleInput === 7) {
-    resultImage = badResult;
+    resultImage = <Image source={require('../assets/images/badresult.png')} style={styles.headerImage} />;
     resultHeader = 'Sound the Alarm';
     resultText = <Text>When you report 1 or 7 your symptoms are severe. Here are the meals you ate in the last 30-40 hours.</Text>;
     specialText = <Text style={styles.redText}>If your issues persist, please seek professional medical attention.</Text>
   } else if (scaleInput === 3 || scaleInput === 4) {
-    resultImage = goodResult;
+    resultImage = <Image source={require('../assets/images/goodresult.png')} style={styles.headerImage} />;
     resultHeader = 'In the Zone';
     resultText = <Text style={[styles.h3, styles.textLeft]}>When you report 3 or 4 this means you’re in a healthy range.</Text>;
     resultSuspectMeals = null;
     actionText = null;
     specialText = <Text style={styles.h3}>Keep it up!</Text>;
   } else {
-    resultImage = okayResult;
+    resultImage = <Image source={require('../assets/images/okayresult.png')} style={styles.headerImage} />;
     resultHeader = 'Room to Improve';
     resultText = <Text>When you report 2, 5 or 6 your symptoms are moderate. Here are the meals you ate in the last 30 - 40 hours.</Text>;
   }
@@ -87,7 +83,7 @@ export default function TrackSubmit(props) {
       style={styles.gradient}>
       <View style={styles.container}>
         <View style={styles.flex}>
-          <Image source={resultImage} style={styles.headerImage} />
+          {resultImage}
           <Text style={styles.h1}>{resultHeader}</Text>
           <TouchableOpacity onPress={() => props.navigation.navigate('Home')} >
             <Text style={styles.btn}>GO BACK</Text>
@@ -97,7 +93,7 @@ export default function TrackSubmit(props) {
         {resultSuspectMeals}
         {actionText}
         {specialText}
-        <Image source={footer} style={styles.footer} />
+        <Image source={require('../assets/images/footer.png')} style={styles.footer} />
       </View>
     </LinearGradient>
   );
